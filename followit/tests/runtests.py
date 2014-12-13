b3a8dcf2a1ef959174c8ad0bcabdc8a88ea7b894
Collin Anderson <cmawebsite@gmail.com>
2014-12-12 22:09:09 -0500

    the first step for django 1.7 support

--- followit/tests/runtests.py
+++ followit/tests/runtests.py
@@ -1,6 +1,11 @@
 import os, sys
 sys.path.append('.')
 os.environ['DJANGO_SETTINGS_MODULE'] = 'followit.tests.settings'
+import django
+try:
+    django.setup()
+except AttributeError:  # django < 1.7
+    pass
 from django.test.runner import DiscoverRunner
 failures = DiscoverRunner().run_tests(['tests.FollowerTests',], verbosity = 1)
 if failures:
