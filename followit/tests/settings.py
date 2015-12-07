f1c866d492cf68d5cbc3c0713f628cb18b83b178
Evgeny Fadeev <evgeny.fadeev@gmail.com>
2015-12-07 08:38:48 -0300

    test cases pass

--- followit/tests/settings.py
+++ followit/tests/settings.py
@@ -13,7 +13,7 @@ MIDDLEWARE_CLASSES = ()
 
 SECRET_KEY = ''.join([random.choice(string.ascii_letters) for x in range(40)])
 
-TEST_RUNNER = 'django.test.simple.DjangoTestSuiteRunner'
+TEST_RUNNER = 'django.test.runner.DiscoverRunner'
 
 INSTALLED_APPS = (
     'django.contrib.auth',
